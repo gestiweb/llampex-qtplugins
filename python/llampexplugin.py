6c0f73b0f8bf5b0f60398f8cf88f09fe7e1faf16
David Martinez Marti <desarrollo@gestiweb.com>
2011-03-14 17:00:03 +0100

    Initial Commit. copied sources from DropBox.

---
+++ python/llampexplugin.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+from PyQt4 import QtGui, QtDesigner
+from llampexwidgets import FLClock, FLFieldDB
+from base64 import b64decode
+clock32png = QtGui.QPixmap()
+clock32png.loadFromData(
+                b64decode(
+                'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A/wD/oL2nkwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9sDBBQBG/S+WMoAAAnBSURBVFjDrZd7bNzVlcc/9/eYp98e'
+                'x488ICXEtCFBiLZUPMxuy4aAIFRii3ZDSBQVxCNt1YjuH5sSqWIflbKbtixNoZCtQ5pQtYVkNwm4LE0DoRCHtEkd4yQmiu3YiSe2xzOe8Xjm95jfPfvHYG/SGrpo90jn9zs6Ouee7z333nPuhT9DN910E6+88sofq+MbNmy44bnn'
+                'nluxdevW5Y8++ugywLjU4Pnnn+f/TC+88MKM/NBDDy3r7Oz86enTp51MJiOO41zG4+Pj0tvbm+rs7Hx68eLFjdN+mzdv/uSBH3jgAUQEgNWrV1/T1dV1cmpqSpxiUTzPEdGeTmcmdHIkJcMXU1IqObpUcsV1i1IsFiWbzcqxY8fe'
+                'AKoAOjo6uO222/53wR977LEZee/evVuy2awUi0UR7ekLF8b1P37vdXnqX34lv9h/TM70j8q582nZ+N39svmZ38jWbW9LsVjQvufoYrEoqVRKdu7cuQbgzJkz3H333R8f/MEHH5yROzs73/E8T3zf1SOjE/prf/+K/OtzB2Ukk5eL'
+                '43npPDEoyVReDnedk6HRSUmm8jI0MiHfffqA/PP3fy068LTjFLXjONLR0fEjgOPHj388gOm0HzlypNP3fREJ9PafvScbvvMfMpyalN5zaTk5kJaTAxnpHczKr4+clXe6LsipcxNyciAjJ/vT8sFgWs4lM/KNb++R4yeGdKnkat/3'
+                '5bXXXnsa4KWXXrospjkttLe3c/311/Pqq69uaWtr+4ppmvL0tneUNjR3r7iOVNbF8cEpCY4vFD1NOBJDmRZFT+P4guNrCq4mX/C58YYFvP7maRWLRNTcxgq5cuGnvjB37twT69atO71p0yYOHToEgAJYuHAh/f39rFq16upt27Z9'
+                'EI1GZMfLf1CZ/CTXLV1I3gOUgSij7PDhd5Ycllk06IA5VTa//M/f8/iaW+SKudUqnU7T0NBwmbMBsH37dgCefPLJPdFohFzeZ/vuTppa5jAw5jCW8xnL+qSyHmNZj7Gs+xHsMZb1GcuVSE0GnByc5MYbF/HNp/YpwzAkkUhw+PDh'
+                'HwOsX7/+8qmsXbv20+3t7SeVUvLX63epv7j5KopE0GYIlEnYDAgE3MDENNSf20zlTOgAI/Bwsynqa2p4bNXnmJiYoLa2dmYAIx6PA7Bhw4YnlFLkCwG9A0mGxn2SEz4XMx7JjMvpCw5XNlYQNgP6kpMkMw7JdPFDdso8rcu4JDNu'
+                '2XfCYyqw+Nn+4wBSU1PD7t27vzoNwMrn8yilqKur+wrA1pc61aJPNXA+4xJYBpgaARSKnx4c5I4bmrhmvsGR02nEsFDq8hN0KSkBtIftewTaoftsWi29qo7W1tb7gX8HsFR5BCuRSFQB/Ne7pwlXxsjlPbRtgqFn1kkp2Pe7Cyxb'
+                'WE9zYwVnL2T4YNSlOhYiHi4vjdbCNBZBULqE4XqEYlH2v3mKpVfdTGVl5a0zGQB4+OGHW6PRKJ7ny8XxjNKuJpyIIdoCM0BrIe8GTDo+makS3ckJ6msrMIBEFbxzcgRCJqZlUBm1qQgbxEIWEdtE6QBcnwtD49w0NVnuZPF49DIA'
+                'S5YsSQBkckX1T9+8i7d7zrPzdyPk7QAvEEqBlLeroSAC5x1NlXa547oWmuriaBsOD+YJECYCj4mCwJSABnSJtpYQP9j8N4yP5wCIRCIANuBbAK7rBo5TxLZNftvVz3v9KRxDKJolsA0MVBmAKv+1QFVtiHzg0TtWom9yCuKqbCLl'
+                'Q6BEIQjighVR7HrjBJOpLPfdvuzSolHOQHd3dzKTyeC6vrx1rE8dy+Qxm5swYoIY/M/slUIpg5gSlrY28MbZMSolYCRsYIYViEZEUBoQjdKCGVK8NZTizaEhnrjnZgLfJZkcBijNFKKdO3eedV2Pxvpq1dRUR9gsYYQCjAgYFQZG'
+                'pcKoNDCrLYIKuO/z8zg+VaC2yqRbAqz6MKrWQlVbGFUmZqVR9qswUGGNHdJYlGhorCE3OUmx6IxeVgkBCoXCQCCKO269Fl2YxFQ+ZlhjxsCsNDGrLFSNxdJ5lbjVIRKVBn8wSkQbI9h1NlZtmc2aaRBm2TekMXHxpjy+fOtiDNPC'
+                'cZzf/AmA8fHxXYZp8fDyVnH9EJZfxDJKWDEwK0ysGotSWPira+fiaJ83giIVzTGs+jBmfRirPoxVZ2NV21jVJmaFgRURLOVhFAosvbaVq5vjYlk2J06c2DFTKy6pG1WZTCZbXRGTDduPqJ90/Ap7yZVIUy2qJoYfsbmzoR7DNtk7'
+                'NUksEkYZqryTtAY/QEoB4pTA9WHKRWfyGBfSTPz+LK99/1v8ZWstI6NjesGCBeZlGejp6QHI9fX17dHKVD946FaxVC12KkuToYjbBguiNp9JVPGW6TGvuZr6RIy6RIz6RIz6+jh1iTi1dTFqayNUx20qTJhvGBjJMb54yy3csaxZ'
+                'LDvE+++//3cAK1eunLWvhjOZjFNTXc0HY458eeMLak5rPeNzqrmqpY5z1WEqE1WosIVhmUzXYdGClAJ0waOUK+CPTuIOpogNpXCyNl0/XCciooaGhtJXXHFF/Z+040uu0e6ePXvu0yIsnhPlJ5vWSdfhQaL9Y+TGC8z1DRp8RbOy'
+                'aTJDNIfCtNhhmk2bRkwSPtTmSsRH81hnkiSHNe/+2zoBKBaLbNy48bMAq1atYrY9QE9PD0uWLKGjo+MfVqxY8SQgw1Oa5Zt+ruIyQeO1C6mYnyBcG8eKh2nJOfQpIRqP4OUdnFSO3MAo/V3nuPqaz7D3idsFUL7v097evvKRRx7Z'
+                't2PHDtasWTM7gOmuppRi3759m+68886nTNMEkGePjqhn9x8lPpWiqaUG3zD43t+2sePtHnrTUzjpLAMjBRrmzefb93+BL82PCaAKhQIvvvjiyscff3zfoUOHaGtr+/i7xKJFi2Za65YtW24fGRmRS0i/lxPZ/G5Snn39fXn23UH5'
+                '+u4e+er2I/LjrrQMlco208Z9fX0j99xzz0KAAwcOfLLHydGjR2fkAwcObB4bG7sUiDiOJ4V8QZypgvwxnT9/3nn55ZfXT/tv3br1kwVfu3btrPpdu3bdf+rUqV9evHjxfD6fn3mW5XI5PTw8fKa7u3vbM88888XZfJcvXz7rmB95'
+                'uVu9erXd0tKSiMViEa116OjRo2Yul6vp7e29MpVKJYBqoOZD8wkgP2fOnKzjOEPz5s0bb2trC2zbdnO5XGFwcHD44MGDpdniWB8FYGBgoOQ4Tjoej1tBEJjDw8NmyffHqqqqhvP5fIXv+6EgCAxAGYahQ6HQVDQaLRSLRRfwPM8L'
+                'gEAp5du2XfpES3DXXXfx/0333nvvrPr/BoWlTx10OZEsAAAAAElFTkSuQmCC')
+            ,"png")
+            
+class FLClockPlugin(QtDesigner.QPyDesignerCustomWidgetPlugin):
+
+    # The __init__() method is only used to set up the plugin and define its
+    # initialized variable.
+    def __init__(self, parent=None):
+        super(FLClockPlugin, self).__init__(parent)
+
+        self.initialized = False
+
+    # The initialize() and isInitialized() methods allow the plugin to set up
+    # any required resources, ensuring that this can only happen once for each
+    # plugin.
+    def initialize(self, core):
+
+        if self.initialized:
+            return
+
+        self.initialized = True
+
+    def isInitialized(self):
+
+        return self.initialized
+
+    # This factory method creates new instances of our custom widget with the
+    # appropriate parent.
+    def createWidget(self, parent):
+        return FLClock(parent)
+
+    # This method returns the name of the custom widget class that is provided
+    # by this plugin.
+    def name(self):
+        return "FLClock"
+
+    # Returns the name of the group in Qt Designer's widget box that this
+    # widget belongs to.
+    def group(self):
+        return "Llampex Widgets"
+
+    # Returns the icon used to represent the custom widget in Qt Designer's
+    # widget box.
+    def icon(self):
+        return QtGui.QIcon(clock32png)
+
+    # Returns a short description of the custom widget for use in a tool tip.
+    def toolTip(self):
+        return ""
+
+    # Returns a short description of the custom widget for use in a "What's
+    # This?" help message for the widget.
+    def whatsThis(self):
+        return ""
+
+    # Returns True if the custom widget acts as a container for other widgets;
+    # otherwise returns False. Note that plugins for custom containers also
+    # need to provide an implementation of the QDesignerContainerExtension
+    # interface if they need to add custom editing support to Qt Designer.
+    def isContainer(self):
+        return False
+
+    # Returns the module containing the custom widget class. It may include
+    # a module path.
+    def includeFile(self):
+        return "llampexwidgets"
+        
+
+class FLFieldDBPlugin(QtDesigner.QPyDesignerCustomWidgetPlugin):
+
+    # The __init__() method is only used to set up the plugin and define its
+    # initialized variable.
+    def __init__(self, parent=None):
+        super(FLFieldDBPlugin, self).__init__(parent)
+
+        self.initialized = False
+
+    # The initialize() and isInitialized() methods allow the plugin to set up
+    # any required resources, ensuring that this can only happen once for each
+    # plugin.
+    def initialize(self, core):
+
+        if self.initialized:
+            return
+
+        self.initialized = True
+
+    def isInitialized(self):
+
+        return self.initialized
+
+    # This factory method creates new instances of our custom widget with the
+    # appropriate parent.
+    def createWidget(self, parent):
+        return FLFieldDB(parent)
+
+    # This method returns the name of the custom widget class that is provided
+    # by this plugin.
+    def name(self):
+        return "FLFieldDB"
+
+    # Returns the name of the group in Qt Designer's widget box that this
+    # widget belongs to.
+    def group(self):
+        return "Llampex Widgets"
+
+    # Returns the icon used to represent the custom widget in Qt Designer's
+    # widget box.
+    def icon(self):
+        return QtGui.QIcon(clock32png)
+
+    # Returns a short description of the custom widget for use in a tool tip.
+    def toolTip(self):
+        return ""
+
+    # Returns a short description of the custom widget for use in a "What's
+    # This?" help message for the widget.
+    def whatsThis(self):
+        return ""
+
+    # Returns True if the custom widget acts as a container for other widgets;
+    # otherwise returns False. Note that plugins for custom containers also
+    # need to provide an implementation of the QDesignerContainerExtension
+    # interface if they need to add custom editing support to Qt Designer.
+    def isContainer(self):
+        return False
+
+    # Returns the module containing the custom widget class. It may include
+    # a module path.
+    def includeFile(self):
+        return "llampexwidgets"
+        
---
+++ widgets/llampexwidgets.py
@@ -0,0 +1,446 @@
+#!/usr/bin/env python
+
+from PyQt4 import QtCore, QtGui
+import math
+
+class FLFieldDB(QtGui.QFrame):
+    searchClicked = QtCore.pyqtSignal()
+    enumFieldType = ["auto","string","optionlist","double",
+                "number","uint","stringlist","pixmap","unlock",
+                "serial","bool","date","time"]
+                
+    
+    """
+        AbanQfieldType / PostgreSQL Type        / AbanQWidget 
+        'string'       : 'character varying'    : flabel-sbutton-lineedit
+        'optionlist'   : 'character varying'    : flabel-combobox/listview
+        'double'       : 'double precision'     : flabel-DoubleSpinbox/LineEdit
+        'number'       : 'integer' ???          <<  
+        'int'          : 'integer',             : flabel-Spinbox/LineEdit
+        'uint'         : 'integer',            
+        'unit'         : 'smallint',            -------------  >>
+        'stringlist'   : 'text',                : flabel-plainTextEdit/textEdit
+        'pixmap'       : 'text',                : flabel-graphicsView
+        'unlock'       : 'boolean',             : ???? flabel-pushbutton(checkable) ???
+        'serial'       : 'serial',              : ???? flabel-spinBox ???
+        'bool'         : 'bool',                : checkbox  |  flabel-checkbox
+        'date'         : 'date',                : flabel-dateedit(w/ or w/o calendar) / flabel-calendar
+        'time'         : 'time',                : flabel-timeedit
+        
+        
+        Several controls or behaviors were impossible for AbanQ:
+        
+        FieldName Migration:
+            - To alter a column name without losing its data.
+        
+        ReadOnly:
+            - Disable VS ReadOnly
+            - ColorScheme for ReadOnly
+        
+        Group Fields by typology:
+            - References, codes
+            - Basic Descriptions
+            - Prices / Costs
+            - Options...
+        -->>> And associate these groups to stylesheets that may change control colors, like background.
+                ... this may help the user find what kind of information the form is requesting
+                ---- readonly may add some changes
+                ---- required fields may add some changes
+                
+        MultiFieldControls:
+            - Controls that require a group of fields (multilingual, options, multicheckbox)
+            
+        Mainly a field is composed of:
+            - lblFieldName: QLabel with Field Name
+            - tbnSearch: QToolButton for search possible values for the field
+            - txtField: FieldControl, depending on which fieldtype is.
+            
+        All controls (label, search button, text field):
+            - Enabled :: Child controls get siabled when the parent is disabled too. 
+                (so only makes sense for disabling some of them, not all)
+            - Size Policy ?
+            - Minimum / Maximum Sizes ?
+            - Pallete ? 
+            - Fonts / Bold ?
+            - Tooltip ?
+            - locale ?
+            
+        FieldName Label:
+            - Manually set the label text, including rich text features.
+            - Text replaceable by Pixmap / icon
+                - scaledContents Yes/No for stretch the pixmap
+            - manually determined size of the control or size policies.
+            - manually set the text/pixmap aligment (left-right-top-bottom-center-middle)
+            - capability of word wrapping
+            - manually change textInteractionFlags (text selectable, editable, open external links..)
+                * what means to edit a FieldName in a form? makes sense?
+        
+        Search Button:
+            - May be other class or button style instead? QPushButton, QToolButton...
+            - Is the only button action possible? maybe there more than one button? different actions?
+            - May the icon change, text instead
+            - May set the textflow settings of text+icon
+            - Checkable, makes sense?
+            - Popup-menu, more options (buttons) can appear when pressing and holding the button
+            - configurable shortcuts
+        
+        All Text Field Edits:
+            - Readonly yes/no
+            - Frame yes/no
+            - aligment
+        
+        FieldType String: (Assume string is for one-line string QLineEdit)
+            - Other PostgreSQL datatypes for this could be:
+                * text, for infinite one-line text
+                * char, for fixed-width short text
+                * int/double/number , with inputmask 0000...  and conversion to number on save.
+            - patterns / formats?
+                - InputMask, 999999A
+                - Validators (we should precreate validators or... )
+            - echoMode for passwords and so.
+            - auto md5/sha1 (hash) on save? cipher on save?
+            - placeHolderText (like "Write here your fielddata") --> collapse field label with placeholder text
+            - what about multilingual texts? they may span multiple fields in database.
+            ** main problems:
+                * with inputmask, the control seems to do it wrong, maybe confuses the user
+                
+        FieldType OptionString: (QComboBox)
+            - Editable: A QLineEdit with dropdown of options. Remembers previous entered items.
+                - InsertPolicy: AtBottom, AtCurrent, Alphabetically.
+            - May allow icons along with options
+            
+        FieldType OptionString: (QListWidget)
+            
+        FieldType Uint: (QSpinBox) (also, QDial, QSlider)
+            - wrapping ( 99.99 -> 0.0)
+            - buttonSymbols -> arrows, plusminus, none
+            - specialValueText (Maybe for mapping None? )
+            - accelerated yes/no
+            - correctionMode -> previousvalue, nearestvalue
+            - keyboardtraking yes/no
+            ::::::::::::
+            - suffix, prefix
+            - minimum, maximum
+            - singleStep
+            :::::
+            ** main problems:
+                * awful control to fill data with keyboard.
+        
+        FieldType Double: (QDoubleSpinBox)
+            - wrapping ( 99.99 -> 0.0)
+            - buttonSymbols -> arrows, plusminus, none
+            - specialValueText (Maybe for mapping None? )
+            - accelerated yes/no
+            - correctionMode -> previousvalue, nearestvalue
+            - keyboardtraking yes/no
+            ::::::::::::
+            - suffix, prefix
+            - minimum, maximum
+            - singleStep
+            - decimals 
+            :::::
+            ** main problems:
+                * awful control to fill data with keyboard.
+        
+        Control Layout:
+            - Horizontal / Vertical
+            - Straight / Reversed Order
+            
+        modes:
+            -> Auto (Automatically selects the proper mode for the fieldType)
+            -> LineEdit
+    """
+    #modeList = ["Auto","LineEdit","Multiline","Checkbox","Radio]
+    
+    def __init__(self, parent=None):
+        super(FLFieldDB, self).__init__(parent)
+        self._layout = QtGui.QHBoxLayout()
+        self._labelSuffix = ": "
+        self._label = QtGui.QLabel("FieldName" + self._labelSuffix)
+        self._button = QtGui.QToolButton() # TODO: change to tool button.
+        self._button.setText("::")
+        self._editors = {
+            'line' : QtGui.QLineEdit("FieldContent"),
+            'checkbox' : QtGui.QCheckBox("FieldContent"),
+            'combobox' : QtGui.QComboBox(),
+            'spinbox' : QtGui.QSpinBox(),
+        }
+        self._editor = self._editors['line']
+        self._layout.addWidget(self._label)
+        self._layout.addWidget(self._button)
+        for editor in self._editors.values():
+            editor.setVisible(False)
+            editor.setSizePolicy(QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Minimum)
+            self._layout.addWidget(editor)
+        self._label.setSizePolicy(QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Minimum)
+        self._button.setSizePolicy(QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Minimum)
+        self._editor.setVisible(True)
+        self._layout.setSpacing(0)
+        self._margin = 3
+        self._layout.setContentsMargins(self._margin,self._margin,self._margin,self._margin)
+        self.setLayout(self._layout)
+        
+        self._fieldName = ""
+        self.connect(self._button, QtCore.SIGNAL("released()"),
+                        self.searchClicked)
+        self._mode = 0
+        self._fieldType = 0
+    
+    def getFieldName(self):
+        return self._fieldName
+    
+    @QtCore.pyqtSlot(str)
+    def setFieldName(self,value):
+        self._fieldName = value
+    
+    def resetFieldName(self):
+        self._fieldName = ""
+    
+    def setMargin(self,value): 
+        self._margin = value
+        self._layout.setContentsMargins(self._margin,self._margin,self._margin,self._margin)
+    
+    def getMargin(self): return self._margin
+
+    @QtCore.pyqtSlot(str)
+    def setLabelSuffix(self,value): self._labelSuffix = value; self.setLabelText(self.getLabelText())
+    def getLabelSuffix(self): return self._labelSuffix
+    
+    @QtCore.pyqtSlot(str)
+    def setLabelText(self,value): return self._label.setText(value+self._labelSuffix)
+    def getLabelText(self): 
+        if self._labelSuffix:
+            x = len(self._labelSuffix)
+            return self._label.text()[:-x] 
+        else:
+            return self._label.text()
+
+    @QtCore.pyqtSlot(int)
+    def setLabelMinWidth(self,value): 
+        self._label.setMinimumWidth(value)
+        
+    def getLabelMinWidth(self): return self._label.minimumWidth()
+
+    @QtCore.pyqtSlot(str)
+    def setButtonText(self,value): return self._button.setText(value)
+    def getButtonText(self): return self._button.text()
+    
+    def getEditorText(self): return self._editor.text()
+    
+    @QtCore.pyqtSlot(str)
+    def setEditorText(self,value): return self._editor.setText(value)
+    
+    @QtCore.pyqtSlot()
+    def resetEditorText(self): return self._editor.setText("")
+    
+    @QtCore.pyqtSlot(bool)
+    def setLabelVisible(self,value): self._label.setVisible(value)
+    def isLabelVisible(self): return self._label.isVisible()
+    
+    @QtCore.pyqtSlot(bool)
+    def setEditorVisible(self,value): self._editor.setVisible(value)
+    def isEditorVisible(self): return self._editor.isVisible()
+    
+    @QtCore.pyqtSlot(bool)
+    def setButtonVisible(self,value): self._button.setVisible(value)
+    def isButtonVisible(self): return self._button.isVisible()
+    
+    margin = QtCore.pyqtProperty(int, getMargin, setMargin, None)
+    
+    fieldName = QtCore.pyqtProperty(str, getFieldName, setFieldName, resetFieldName)
+
+    labelSuffix = QtCore.pyqtProperty(str, getLabelSuffix, setLabelSuffix, None)
+    labelMinWidth = QtCore.pyqtProperty(int, getLabelMinWidth, setLabelMinWidth, None)
+    
+    editorText = QtCore.pyqtProperty(str, getEditorText, setEditorText, resetEditorText)
+    labelText = QtCore.pyqtProperty(str, getLabelText, setLabelText, None)
+    buttonText = QtCore.pyqtProperty(str, getButtonText, setButtonText, None)
+    
+    labelVisible = QtCore.pyqtProperty(bool, isLabelVisible, setLabelVisible, None)
+    editorVisible = QtCore.pyqtProperty(bool, isEditorVisible, setEditorVisible, None)
+    buttonVisible = QtCore.pyqtProperty(bool, isButtonVisible, setButtonVisible, None)
+    
+    def getFieldType(self): return self._fieldType
+    def setFieldType(self,value): 
+        if value < 0 or value >= len(self.enumFieldType): return False
+        self._fieldType = value
+        self.updateFieldType()
+        
+    def updateFieldType(self):
+        typeName = self.enumFieldType[self._fieldType]
+        neweditor = self._editors['line']
+        if typeName == 'bool':  neweditor = self._editors['checkbox']
+        if typeName == 'optionlist':  neweditor = self._editors['combobox']
+        if typeName == 'uint':  neweditor = self._editors['spinbox']
+        
+        if neweditor != self._editor:
+            print "typeName:", typeName, "newEditor:", type(neweditor)
+            self._editor.setVisible(False)
+            self._editor = neweditor
+            self._editor.setVisible(True)
+    
+    def getFieldTypeName(self): return self.enumFieldType[self._fieldType]
+    def setFieldTypeName(self,value): 
+        try:
+            self._fieldType = self.enumFieldType.index(value)
+            self.updateFieldType()
+        except ValueError:
+            return False
+    
+    fieldType = QtCore.pyqtProperty(int, getFieldType, setFieldType, None)
+    fieldTypeName = QtCore.pyqtProperty(str, getFieldTypeName, setFieldTypeName, None)
+
+
+
+
+
+
+class FLClock(QtGui.QWidget):
+    # Emitted when the clock's time changes.
+    timeChanged = QtCore.pyqtSignal(QtCore.QTime)
+
+    # Emitted when the clock's time zone changes.
+    timeZoneChanged = QtCore.pyqtSignal(int)
+
+    def __init__(self, parent=None):
+
+        super(FLClock, self).__init__(parent)
+
+        self.timeZoneOffset = 0
+
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update)
+        timer.timeout.connect(self.updateTime)
+        timer.start(60)
+
+        self.setWindowTitle(QtCore.QObject.tr(self, "Analog Clock"))
+        self.resize(200, 200)
+
+        self.hourHand = QtGui.QPolygon([
+            QtCore.QPoint(7, 8),
+            QtCore.QPoint(-7, 8),
+            QtCore.QPoint(0, -40)
+        ])
+        self.minuteHand = QtGui.QPolygon([
+            QtCore.QPoint(7, 8),
+            QtCore.QPoint(-7, 8),
+            QtCore.QPoint(0, -70)
+        ])
+        self.secondHand = QtGui.QPolygon([
+            QtCore.QPoint(3, 2),
+            QtCore.QPoint(0, 6),
+            QtCore.QPoint(-3, 2),
+            QtCore.QPoint(0, -100)
+        ])
+
+        self.hourColor = QtGui.QColor(0, 127, 0)
+        self.minuteColor = QtGui.QColor(0, 127, 127, 191)
+        self.secondColor = QtGui.QColor(255, 100, 21, 190)
+        self.hourPen = QtGui.QPen(QtGui.QBrush(QtCore.Qt.Dense5Pattern),3)
+
+    def paintEvent(self, event):
+
+        side = min(self.width(), self.height())
+        time = QtCore.QTime.currentTime()
+        time = time.addSecs(self.timeZoneOffset * 3600)
+
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.translate(self.width() / 2, self.height() / 2)
+        painter.scale(side / 200.0, side / 200.0)
+
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtGui.QBrush(self.hourColor))
+
+        painter.save()
+        painter.rotate(30.0 * ((time.hour() + time.minute() / 60.0)))
+        painter.drawConvexPolygon(self.hourHand)
+        painter.restore()
+
+        painter.setPen(self.hourColor)
+        painter.setPen(self.hourPen)
+
+        for i in range(0, 12):
+            painter.drawLine(88, 0, 96, 0)
+            painter.rotate(30.0)
+
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtGui.QBrush(self.minuteColor))
+
+        painter.save()
+        painter.rotate(6.0 * (time.minute() + time.second() / 60.0))
+        painter.drawConvexPolygon(self.minuteHand)
+        painter.restore()
+
+        painter.setPen(QtCore.Qt.NoPen)
+        painter.setBrush(QtGui.QBrush(self.secondColor))
+
+        painter.save()
+        painter.rotate(6.0 * (time.second() + (time.msec() / 1000.0) ** 3.0 ))
+        painter.drawConvexPolygon(self.secondHand)
+        painter.restore()
+        
+        painter.setPen(QtGui.QPen(self.minuteColor))
+
+        for j in range(0, 60):
+            if (j % 5) != 0:
+                painter.drawLine(92, 0, 96, 0)
+            painter.rotate(6.0)
+
+        painter.end()
+
+    def minimumSizeHint(self):
+
+        return QtCore.QSize(50, 50)
+
+    def sizeHint(self):
+
+        return QtCore.QSize(100, 100)
+
+    def updateTime(self):
+
+        self.timeChanged.emit(QtCore.QTime.currentTime())
+
+    # The timeZone property is implemented using the getTimeZone() getter
+    # method, the setTimeZone() setter method, and the resetTimeZone() method.
+
+    # The getter just returns the internal time zone value.
+    def getTimeZone(self):
+
+        return self.timeZoneOffset
+
+    # The setTimeZone() method is also defined to be a slot. The @pyqtSlot
+    # decorator is used to tell PyQt which argument type the method expects,
+    # and is especially useful when you want to define slots with the same
+    # name that accept different argument types.
+
+    @QtCore.pyqtSlot(int)
+    def setTimeZone(self, value):
+
+        self.timeZoneOffset = value
+        self.timeZoneChanged.emit(value)
+        self.update()
+
+    # Qt's property system supports properties that can be reset to their
+    # original values. This method enables the timeZone property to be reset.
+    def resetTimeZone(self):
+
+        self.timeZoneOffset = 0
+        self.timeZoneChanged.emit(0)
+        self.update()
+
+    # Qt-style properties are defined differently to Python's properties.
+    # To declare a property, we call pyqtProperty() to specify the type and,
+    # in this case, getter, setter and resetter methods.
+    timeZone = QtCore.pyqtProperty(int, getTimeZone, setTimeZone, resetTimeZone)
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    app = QtGui.QApplication(sys.argv)
+    field1 = FLFieldDB()
+    field1.show()
+    sys.exit(app.exec_())
